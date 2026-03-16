1f65ee7513f198962769c9024f9ed3fec9a2a9ea
Your Name <you@example.com>
2026-03-16 15:20:11 +0530

    build commited 1

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,21 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./",   // 👈 important for static hosting
+
   server: {
     host: "::",
   },
+
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
-
